debcc23eb7c9fbb4029ab28ed580635a2d81f279
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-12-23 04:40:58 -0500

    file path

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     // Change the filepath here to one of
     // dj38.tsp, rw1621.tsp, eg7146.tsp, or ar9152.tsp
 
-    string filename = "/Users/raasikh/CLionProjects/csci335_3/rw1621.tsp";
+    string filename = "rw1621.tsp";
     nearestNeighbor(filename);
     greedyTSP(filename);
 
